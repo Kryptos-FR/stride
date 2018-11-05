111a383ccabbcbd8a058d140fe4466ed5ad312f4
Nicolas Musset <musset.nicolas@gmail.com>
2018-11-05 21:26:29 +0900

    [GameStudio2] Add Xenko.GameStudio2 project
    - based on AvaloniaUI

---
+++ sources/editor/Xenko.GameStudio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using Xenko.GameStudio2.ViewModels;
+using Xenko.GameStudio2.Views;
+
+namespace Xenko.GameStudio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
---
+++ sources/editor/Xenko.GameStudio2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Xenko.GameStudio2.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
