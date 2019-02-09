e713f872e93366178a87504c6528f377ca8c18a3
Nicolas Musset <musset.nicolas@gmail.com>
2019-02-09 18:06:44 +0900

    [Editor] Package Manager (WIP)

---
+++ sources/editor/Xenko.Core.Assets.Editor/View/PackageManager.xaml.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Xenko contributors (https://xenko.com)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+using System.Windows;
+
+namespace Xenko.Core.Assets.Editor.View
+{
+    /// <summary>
+    /// Interaction logic for PackageExplorer.xaml
+    /// </summary>
+    public partial class PackageManager : Window
+    {
+        public PackageManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ sources/editor/Xenko.Core.Assets.Editor/ViewModel/PackageManagerViewModel.cs
@@ -0,0 +1,13 @@
+using Xenko.Core.Presentation.ViewModel;
+
+namespace Xenko.Core.Assets.Editor.ViewModel
+{
+    internal sealed class PackageManagerViewModel : DispatcherViewModel
+    {
+        public PackageManagerViewModel()
+            : base(ViewModelServiceProvider.NullServiceProvider)
+        {
+
+        }
+    }
+}
