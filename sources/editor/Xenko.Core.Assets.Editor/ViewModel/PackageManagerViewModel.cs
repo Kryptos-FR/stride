4698b9a9183318eec588646b7669766936afc25f
Nicolas Musset <musset.nicolas@gmail.com>
2019-02-11 16:01:59 +0900

    [Editor] Package Manager (WIP)

--- sources/editor/Xenko.Core.Assets.Editor/ViewModel/PackageManagerViewModel.cs
+++ sources/editor/Xenko.Core.Assets.Editor/ViewModel/PackageManagerViewModel.cs
@@ -1,13 +1,59 @@
+using Xenko.Core.Presentation.Collections;
 using Xenko.Core.Presentation.ViewModel;
 
 namespace Xenko.Core.Assets.Editor.ViewModel
 {
     internal sealed class PackageManagerViewModel : DispatcherViewModel
     {
+        private PackageDescriptionViewModel _currentPackage;
+        private readonly IObservableCollection<PackageDescriptionViewModel> _packages = new ObservableList<PackageDescriptionViewModel>();
+
         public PackageManagerViewModel()
-            : base(ViewModelServiceProvider.NullServiceProvider)
+            : base(ViewModelServiceProvider.NullServiceProvider) // FIXME
         {
 
+        }
+
+        public PackageDescriptionViewModel CurrentPackage
+        {
+            get { return _currentPackage; }
+            set { SetValue(ref _currentPackage, value); }
+        }
+
+        public IObservableCollection<PackageDescriptionViewModel> Packages
+        {
+            get { return _packages; }
+        }
+    }
+
+    internal sealed class PackageDescriptionViewModel : DispatcherViewModel
+    {
+        private string _description;
+        private object _icon;
+        private string _version;
+
+        public PackageDescriptionViewModel()
+            : base(ViewModelServiceProvider.NullServiceProvider) // FIXME
+        {
+
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { SetValue(ref _description, value); }
+        }
+
+        public object Icon
+        {
+            get { return _icon; }
+            set { SetValue(ref _icon, value); }
+        }
+
+        public string Version
+        {
+            get { return _version; }
+            set { SetValue(ref _version, value); }
         }
     }
 }
